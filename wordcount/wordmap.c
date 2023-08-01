bf7ce96ec47eac7e3991eb709ef2be94085dca65
Marwane <taroua.marwane@gmail.com>
2023-08-01 14:43:34 -0400

    adding makefile

--- wordcount/wordmap.c
+++ wordcount/wordmap.c
@@ -103,22 +103,3 @@ void printMap(word_map wordMap){
     }
     printf("---End---\n");
 }
-
-int main(){
-
-    word_map table = initWordMap();
-
-    char* w4 = "Howdy";
-    char* w5 = "Hackle";
-    char* w6 = "Dude";
-    char* w7 = "Animal";
-    
-    insertWord(table, w4,11);
-    insertWord(table,w5,3);
-    insertWord(table,w6,5);
-    insertWord(table,w7,6);
-    printMap(table);
-    deleteWord(table,w4);
-    printMap(table);
-    return 0;
-}
